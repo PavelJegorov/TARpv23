e8f16431664894725aeda5bcc3902a64ba1ee270
opilane <opilane@HP-CZC2349HTG>
2024-01-11 12:14:26 +0200

    try accept

--- TARpv23.py
+++ TARpv23.py
@@ -1,3 +1,14 @@
+#from importlib.resources import Package
+#from random import *
+#from tkinter import Pack
+#10
+#P=randint(1,5)
+#hind=12.90
+#hind*=1.1 #hind koos jätatega
+#osa=round(hind/P,2)
+#print("Iga sõber maksab: ",osa)
+
+
 #print("Tere maailm!")
 #nimi=input("Mis on simu nimi?")
 #vanus=int(input("Kui vana sa oled")) #floae()->2.5
@@ -18,9 +29,20 @@
 #print("Muutuja kas_käib_koolis=",kas_käib_koolis,",tüüp on ",type(kas_käib_koolis))
 
 #3
-from random import *
-kokku=randint(10,100)
-print("Kokku: ", kokku)
-mitu=int(input("mitu kommi tahab võtta?"))
-kokku=mitu
-print("Nüüd laua peal on",kokku,"kommid")
+#from random import *
+#kokku=randint(10,100)
+#print("Kokku: ", kokku)
+#mitu=int(input("mitu kommi tahab võtta?"))
+#kokku=mitu
+#print("Nüüd laua peal on",kokku,"kommid")
+
+
+#6
+try:
+    aeg = float(input("Mitu tundi kulus sõiduks")) #aeg ie saa olla 0
+    teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+    kiirus = teepikkus/aeg
+    print("Sinu kiirus oli " + str(kiirus) + " km/h")
+except :
+    print("Viga andmetüübiga")
+
